fac1cce00430723df58da04dc649dfb899f69ebc
Awwal <awwalonpc@gmail.com>
2025-11-28 14:44:04 +0100

    Unsupervised attempt at building rle algorithm.

--- compression/src/process.c
+++ compression/src/process.c
@@ -1,24 +1,56 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define FILE_IN "file.txt"
 #define FILE_OUT "zipped.txt"
-#define MAX_FILE_SIZE 1024 // 1kb max file  
 
 int main() {
     FILE *fileIn = fopen(FILE_IN, "r");
-    FILE *fileOut = fopen(FILE_OUT, "a");
+    FILE *fileOut = fopen(FILE_OUT, "w");
+    
     if (fileIn == NULL || fileOut == NULL ) {
         perror("Could not open file.");
         return 1;
         }
-    int ch; 
-    for (int i = 0; i < MAX_FILE_SIZE; i++) {
-        ch = fgetc(fileIn);
-        if (ch == EOF) {
+        
+    fseek(fileIn, 0, SEEK_END);
+    size_t fileSize = ftell(fileIn);
+    fseek(fileIn, 0, SEEK_SET);
+    
+    int count = 0, index = 0;
+    char currChar, prevChar;
+    prevChar = '\0';
+    char *zippedChars;
+    zippedChars = malloc(fileSize);
+    
+    if (zippedChars == NULL) {
+        perror("Memory allocation failed.");
+        fclose(fileIn);
+        }
+    
+    for (int i = 0; i < fileSize - 1; i++) {
+        currChar = fgetc(fileIn);
+        if ((int)currChar == EOF) {
             break;
             }
-        fputc((char)ch, fileOut);
+        if(currChar == prevChar) {
+            prevChar = currChar;
+            count++;
+            continue;
+            }
+        if (currChar != prevChar && prevChar != '\0') {
+            zippedChars[index] = count;
+            index++;
+            zippedChars[index] = currChar;
+            index++;
+            count = 0;
+            prevChar = currChar;
+            count++;
+            }            
         }
+    printf("%d, %d \n", count, index);
+    printf("%s \n", zippedChars);
+    fputs(zippedChars, fileOut);
     fclose(fileIn);
     fclose(fileOut);
     return 0;
