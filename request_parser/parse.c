733b2de7171bebb82b9b5d9011f34594c8ee441d
Awwal <awwalonpc@gmail.com>
2025-12-17 22:00:44 +0100

    request_parser: modified the parser with functionalities closer to real-world production code.

--- request_parser/parse.c
+++ request_parser/parse.c
@@ -1,62 +1,105 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define GET 0
-#define POST 1
-#define PUT 2
+typedef struct {
+    char key[64];
+    char value[256];
+    } Header;
 
-#include <stdio.h>
-#include <string.h>
+typedef struct {
+    char method[16];
+    char path[32];
+    char version[16];
+    Header headers[32];
+    int header_count;
+    char *body;
+    } HttpRequest;
 
-#define GET 0
-#define POST 1
-#define PUT 2
-
-void parse_path(char *path, int index) {
-    char *ext; 
+HttpRequest* parse_request(char *request) {
+    HttpRequest *req = calloc(1, sizeof(HttpRequest));
+    if (!req) {
+	perror("calloc() failed.");
+	exit(EXIT_FAILURE);
+	}
     
-    const char *http_method[] = {"GET", "POST", "PUT"};
+    char *body = strstr(request, "\r\n\r\n");
+    req->body = body + 4;
+    int header_portion = body - request;
     
-    if ((ext = strrchr(path, '.')) != NULL) {
-        printf("Extension of the file in path %s: %s\n", path, ext);
-        if (!strcmp(ext, ".jpg") || !strcmp(ext, ".png")) {
-            printf("The file is an image file. Method: %s. \n", http_method[index]);
-            }
-        else if (!strcmp(ext, ".html")){
-            printf("The file is an html file. \n");
-            }
-        else if (!strcmp(ext, ".css")) {
-            printf("The file is a css file. \n");
-            }
-        return;
-        }
-    if ((ext = strrchr(path, '?')) != NULL) {
-        printf("Method: %s \nParameter: %s\n", http_method[index], ext);
-        }
-    return;
+    char *header_copy = malloc(header_portion + 1);
+    if (!header_copy) {
+	perror("malloc() failed.");
+	free(req);
+	exit(EXIT_FAILURE);
+	}
+	
+    strncpy(header_copy, request, header_portion);
+    header_copy[header_portion] = '\0';
+    
+    char *line = strtok(header_copy, "\r\n"); 
+    sscanf(line, "%15s %31s %15s", req->method, req->path, req->version);
+    
+    printf("==== Request Line ====\n");
+    printf("Method: %s \n", req->method);
+    printf("Path: %s \n", req->path);
+    printf("Version: %s \n", req->version);
+    printf("\n");
+    
+    printf("==== Headers ====\n");
+    while ((line = strtok(NULL, "\r\n")) != NULL) {
+	printf("%s \n", line);
+	char *colon = strchr(line, ':');
+	if (colon) {
+	    *colon = '\0';
+	    char *key = line;
+	    char *value = colon + 1;
+	    
+	    while (*value == ' ') value++;
+	    
+	    snprintf(req->headers[req->header_count].key, 
+		     sizeof(req->headers[req->header_count].key),
+		     "%s", key);
+		     
+	    snprintf(req->headers[req->header_count].value, 
+		     sizeof(req->headers[req->header_count].value),
+		     "%s", value);
+		     
+	    req->header_count++;
+	    }
+	}
+    printf("\n");
+    
+    printf("==== Body ====\n");
+    printf("%s \n", req->body);
+    
+    free(header_copy);
+    return req;
     }
-int parse_method(const char *method_buffer) {    
-    if (strcmp(method_buffer,"PUT") == 0) return PUT;
-    if (strcmp(method_buffer,"GET") == 0) return GET;
-    if (strcmp(method_buffer,"POST") == 0) return POST;
-        
-    printf("Unrecognised method. \n");
-    return -1;
+
+const char* get_header(HttpRequest *req, const char *key) {
+    for (int i = 0; i < req->header_count; i++){
+	if (strcmp(req->headers[i].key, key) == 0) {
+	    return req->headers[i].value;
+	    }
+	}
+    return NULL;
     }
     
 int main() {
     
-    char *request = "POST api/users?id=123 HTTP/1.1";
-    // POST api/users?id=123 HTTP/1.1
-    char method[16];
-    char path[32];
-    char version[16];
+    char *request = 
+        "POST /api/users HTTP/1.1\r\n"
+        "Host: localhost:8080\r\n"
+        "Content-Type: application/json\r\n"
+        "Content-Length: 27\r\n"
+        "\r\n"
+	"{\"name\":\"John\",\"age\":30}";
+	
+    HttpRequest *req = parse_request(request);
     
-    sscanf(request, "%s %s %s", method, path, version);
-    int index = parse_method(method);
-    parse_path(path, index);
-    
+    const char *content_type = get_header(req, "Content-Type");
+    printf("%s \n", content_type);
+    free(req);
     return 0;
     }
-
-
